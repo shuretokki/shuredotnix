cdb7dac82e1f456f4a9b6bceeac985b6d8ae0fbc
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-27 14:50:07 +0700

    autostart swayosd server

--- library/display/autostart.nix
+++ library/display/autostart.nix
@@ -2,6 +2,7 @@
   exec-once = [
     "${pkgs.vicinae}/bin/vicinae daemon"
     "hyprsunset"
+    "swayosd-server"
     "wl-paste --type text --watch cliphist store"
     "wl-paste --type image --watch cliphist store"
   ];
