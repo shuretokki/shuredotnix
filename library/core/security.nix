4e7d3d0e45611e838c4afd8b41f617bf68a25eba
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-27 20:21:47 +0700

    add security services

---
+++ library/core/security.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }: {
+  security.polkit.enable = true;
+
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.login.enableGnomeKeyring = true;
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.waylandFrontend = true;
+    fcitx5.addons = with pkgs; [
+      fcitx5-gtk
+      fcitx5-lua
+    ];
+  };
+
+  # System-wide environment variables
+  # for Fcitx5
+  environment.variables = {
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+  };
+}
