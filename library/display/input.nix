418a3f83fbfa02d953f24ffe86c10104e64d1619
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-27 14:49:47 +0700

    refine hypr input

--- library/display/input.nix
+++ library/display/input.nix
@@ -2,8 +2,21 @@
   input = {
     kb_layout = "us";
     follow_mouse = 1;
+    sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
+    accel_profile = "flat";
+    
+    repeat_rate = 50;
+    repeat_delay = 300;
+
     touchpad = {
       natural_scroll = true;
+      tap-to-click = true;
+      drag_lock = true;
     };
+  };
+
+  gestures = {
+    workspace_swipe = true;
+    workspace_swipe_fingers = 3;
   };
 }
