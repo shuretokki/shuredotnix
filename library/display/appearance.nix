6fddce8b5c6c5c5a7745dd6d599b00d4b0b62071
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-27 14:37:59 +0700

    refine hypr animations

--- library/display/appearance.nix
+++ library/display/appearance.nix
@@ -40,11 +40,35 @@ in {
 
     bezier = [
       "f, 0.05, 0.9, 0.1, 1"
+      "md3_standard, 0.2, 0, 0, 1"
+      "md3_decel, 0.05, 0.7, 0.1, 1"
+      "md3_accel, 0.3, 0, 0.8, 0.15"
+      "overshot, 0.05, 0.9, 0.1, 1.1"
+      "crazy, 0.1, 1.5, 0.76, 0.92"
+      "hyprnostretch, 0.05, 0.9, 0.1, 1.0"
+      "menu_decel, 0.1, 1, 0, 1"
+      "menu_accel, 0.38, 0.04, 1, 0.07"
+      "easeInOutCircle, 0.85, 0, 0.15, 1"
+      "easeOutQuart, 0.25, 1, 0.5, 1"
+      "easeOutExpo, 0.16, 1, 0.3, 1"
+      "softAcDecel, 0.26, 0.26, 0.15, 1"
+      "md2, 0.4, 0, 0.2, 1"
     ];
 
     animation = [
-      "workspaces, 1, 4, f, slide"
-      "windowsMove, 1, 4, f, slide"
+      "windows, 1, 3, md3_decel, popin 60%"
+      "windowsIn, 1, 3, md3_decel, popin 60%"
+      "windowsOut, 1, 3, md3_accel, popin 60%"
+      "border, 1, 10, default"
+      "fade, 1, 3, md3_decel"
+      "layers, 1, 2, md3_decel, slide"
+      "layersIn, 1, 3, menu_decel, slide"
+      "layersOut, 1, 3, menu_accel, slide"
+      "fadeLayersIn, 1, 2, menu_decel"
+      "fadeLayersOut, 1, 3, menu_accel"
+      "workspaces, 1, 7, menu_decel, slide"
+      "workspaces, 1, 2.5, softAcDecel, slide"
+      "specialWorkspace, 1, 3, md3_decel, slidevert"
     ];
   };
 
