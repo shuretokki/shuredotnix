a78e420167df3cbe4ed3b3e767925a9aabc2f149
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-27 14:04:30 +0700

    add gruvbox theme

---
+++ library/display/themes/gruvbox.nix
@@ -0,0 +1,24 @@
+{
+  name = "gruvbox";
+  colors = {
+    bg = "282828";
+    fg = "ebdbb2";
+    red = "fb4934";
+    green = "b8bb26";
+    yellow = "fabd2f";
+    blue = "83a598";
+    purple = "d3869b";
+    aqua = "8ec07c";
+    orange = "fe8019";
+    gray = "928374";
+    
+    active_border = "a89984";
+    inactive_border = "515151";
+  };
+  hyprland = {
+    gaps_in = 4;
+    gaps_out = 12;
+    rounding = 0;
+    border_size = 1;
+  };
+}
