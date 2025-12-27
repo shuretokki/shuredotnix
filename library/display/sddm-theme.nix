2888cec591e040a80050ec397873d7294b1c4732
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-27 21:39:03 +0700

    fix sddm build

--- library/display/sddm-theme.nix
+++ library/display/sddm-theme.nix
@@ -5,16 +5,17 @@ pkgs.stdenv.mkDerivation {
   src = pkgs.fetchFromGitHub {
     owner = "uiriansan";
     repo = "SilentSDDM";
-    rev = "master"; # You might want to use a specific commit hash for reproducibility
+    rev = "master";
     sha256 = "1brvya8r5q03b4jhwv1dqnllaw3iw9d1fnj8ybz9r6gcd1mbf161";
   };
 
   installPhase = ''
     mkdir -p $out/share/sddm/themes/SilentSDDM
     cp -R ./* $out/share/sddm/themes/SilentSDDM
-    ${pkgs.lib.optionalString (background != null) ''
-      cp ${background} $out/share/sddm/themes/SilentSDDM/backgrounds/custom.jpg
+
+    if [ -f "${background}" ]; then
+      cp "${background}" $out/share/sddm/themes/SilentSDDM/backgrounds/custom.jpg
       sed -i 's/background = smoky.jpg/background = custom.jpg/g' $out/share/sddm/themes/SilentSDDM/configs/default.conf
-    ''}
+    fi
   '';
 }
