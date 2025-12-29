7148fb38ffbb75bdbda51593c4ec562b340a208d
Your Name <your.email@example.com>
2025-12-29 14:26:29 +0700

    fix ags entry

--- library/display/ags/app.ts
+++ library/display/ags/app.ts
@@ -1,10 +1,12 @@
 import { App } from "astal/gtk3"
 import Bar from "./widget/Bar"
 import WallpaperPicker from "./widget/WallpaperPicker"
+import style from "./style.scss"
 
 App.start({
     instanceName: "ags",
-    requestHandler(request, res) {
+    css: style,
+    requestHandler(request: string, res: (response: any) => void) {
         res("ok")
     },
     main() {
