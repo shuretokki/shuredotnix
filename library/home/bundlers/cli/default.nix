0d349d30ec155facb4bad42a98dd470dfaeb59e9
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-29 22:37:27 +0700

    add cli bundler

---
+++ library/home/bundlers/cli/default.nix
@@ -0,0 +1,14 @@
+{ ... }: {
+  imports = [
+    ./git.nix
+    ./shell.nix
+    ./starship.nix
+    ./eza.nix
+    ./bat.nix
+    ./fzf.nix
+    ./btop.nix
+    ./zoxide.nix
+    ./direnv.nix
+    ./tools.nix
+  ];
+}
