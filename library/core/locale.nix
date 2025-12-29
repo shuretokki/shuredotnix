363154fcad46af788f52daec201b03836526d076
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-29 22:51:41 +0700

    add core locale

---
+++ library/core/locale.nix
@@ -0,0 +1,15 @@
+{ lib, ... }: {
+  time.timeZone = lib.mkDefault "Asia/Jakarta";
+  i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = lib.mkDefault "en_US.UTF-8";
+    LC_IDENTIFICATION = lib.mkDefault "en_US.UTF-8";
+    LC_MEASUREMENT = lib.mkDefault "en_US.UTF-8";
+    LC_MONETARY = lib.mkDefault "en_US.UTF-8";
+    LC_NAME = lib.mkDefault "en_US.UTF-8";
+    LC_NUMERIC = lib.mkDefault "en_US.UTF-8";
+    LC_PAPER = lib.mkDefault "en_US.UTF-8";
+    LC_TELEPHONE = lib.mkDefault "en_US.UTF-8";
+    LC_TIME = lib.mkDefault "en_US.UTF-8";
+  };
+}
