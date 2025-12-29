63e77f99a9f8a1bd23cbed0648d9d4e782aa76cb
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-29 22:52:28 +0700

    update boot config

--- library/core/boot.nix
+++ library/core/boot.nix
@@ -25,20 +25,4 @@
             '';
         };
     };
-
-    # boot.loader.systemd-boot.configurationLimit = 10;
-
-    time.timeZone = "Asia/Jakarta";
-    i18n.defaultLocale = "en_US.UTF-8";
-    i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-    };
 }
