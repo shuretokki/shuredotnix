22d1dd0ff4d6914d876b855fed46261bf7da16a2
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-29 22:38:44 +0700

    add bundler git

---
+++ library/home/bundlers/cli/git.nix
@@ -0,0 +1,24 @@
+{ lib, vars, ... }: {
+  programs.git = {
+    enable = lib.mkDefault true;
+    lfs.enable = lib.mkDefault true;
+    
+    settings = {
+      user = {
+        name = lib.mkDefault vars.gitname;
+        email = lib.mkDefault vars.email;
+      };
+      init.defaultBranch = lib.mkDefault "main";
+      push.autoSetupRemote = lib.mkDefault true;
+      pull.rebase = lib.mkDefault true;
+      credential.helper = "!gh auth git-credential";
+    };
+    
+    aliases = {
+      co = "checkout";
+      st = "status";
+      lg = "log --oneline --graph --decorate";
+      last = "log -1 HEAD";
+    };
+  };
+}
