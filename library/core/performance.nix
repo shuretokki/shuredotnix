37749d6fea6ecb4722f9c84394b42c140b7a674a
Your Name <your.email@example.com>
2025-12-29 11:03:29 +0700

    optimize zram performance

--- library/core/performance.nix
+++ library/core/performance.nix
@@ -1,10 +1,16 @@
 { config, pkgs, ... }: {
   zramSwap = {
     enable = true;
-    # algorithm = "zstd";
     memoryPercent = 50;
     memoryMax = 8192;
     priority = 100;
+  };
+
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 180;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+    "vm.page-cluster" = 0;
   };
 
   services.earlyoom = {
