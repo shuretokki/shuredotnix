30deb22ae279659e0daf378f0129fcf3634646bb
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-29 22:54:05 +0700

    update cli packages

--- packages/cli.nix
+++ packages/cli.nix
@@ -1,18 +1,11 @@
 { pkgs, ... }: {
     environment.systemPackages = with pkgs; [
-        btop
-        eza
-        zoxide
         wget
         curl
         git
         gh
-        ripgrep
-        fastfetch
-        fzf
-        bat
-        fd
         unzip
         zip
+        fastfetch
     ];
 }
