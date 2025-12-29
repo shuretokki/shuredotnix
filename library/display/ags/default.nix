548d64f4750e06f01bbbef37044f771d38e8ee44
Your Name <your.email@example.com>
2025-12-29 14:26:54 +0700

    update ags nix

---
+++ library/display/ags/default.nix
@@ -0,0 +1,22 @@
+{ pkgs, inputs, vars, ... }: {
+  home-manager.users.${vars.username} = {
+    imports = [ inputs.ags.homeManagerModules.default ];
+
+    programs.ags = {
+      enable = true;
+      configDir = ../ags;
+      extraPackages = with pkgs; [
+        inputs.ags.packages.${pkgs.system}.astal3
+        inputs.ags.packages.${pkgs.system}.hyprland
+        inputs.ags.packages.${pkgs.system}.mpris
+        inputs.ags.packages.${pkgs.system}.network
+        inputs.ags.packages.${pkgs.system}.battery
+        inputs.ags.packages.${pkgs.system}.wp
+        inputs.ags.packages.${pkgs.system}.tray
+        gtksourceview
+        webkitgtk
+        accountsservice
+      ];
+    };
+  };
+}
