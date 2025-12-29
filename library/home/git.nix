c39654b44f34a5175fe365d5046d85e3441c748e
Your Name <your.email@example.com>
2025-12-29 10:51:22 +0700

    move extra config inside setttings block

--- library/home/git.nix
+++ library/home/git.nix
@@ -4,9 +4,10 @@
     settings = {
       user.name = vars.gitname;
       user.email = vars.email;
+      extraConfig = {
+        credential.helper = "!gh auth git-credential";
+      };
     };
-    extraConfig = {
-      credential.helper = "!gh auth git-credential";
-    };
+
   };
 }
