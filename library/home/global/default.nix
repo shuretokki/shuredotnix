185b91f5f95f4ae62dc14129eac2cb60a5cd6e8d
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-31 00:03:00 +0700

    feature flag guards

--- library/home/global/default.nix
+++ library/home/global/default.nix
@@ -1,8 +1,9 @@
+# Home Manager Global Defaults
+# https://nix-community.github.io/home-manager/
 { lib, vars, ... }: {
-  imports = [
-    ../bundlers/cli
-    ../bundlers/desktop
-  ];
+  imports =
+    lib.optionals vars.features.cli [ ../bundlers/cli ]
+    ++ lib.optionals vars.features.desktop [ ../bundlers/desktop ];
 
   xdg.enable = lib.mkDefault true;
 
