031a0e3e7010fd6380f571dbbe4e2fd55ad41254
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-31 23:10:36 +0700

    update stylix config

--- library/display/stylix.nix
+++ library/display/stylix.nix
@@ -1,39 +1,47 @@
 # https://stylix.danth.me/
 { lib, pkgs, vars, ... }:
 let
-  themeData = import (./themes + "/${vars.theme}.nix") { inherit pkgs; };
+  themeDir = ./themes + "/${vars.theme}";
+  themeData = import (themeDir + "/default.nix") { inherit pkgs; };
 in {
   stylix = {
     enable = true;
     autoEnable = true;
 
     base16Scheme = themeData.scheme;
-    image = themeData.wallpaper;
+
+    image = themeDir + "/wallpapers/" + themeData.wallpaperDefault;
     polarity = themeData.polarity;
 
     fonts = {
       monospace = {
         package = pkgs.nerd-fonts.jetbrains-mono;
-        name = lib.mkForce vars.fontMono;
+        name = lib.mkForce themeData.fonts.mono;
       };
       sansSerif = {
         package = pkgs.inter;
-        name = "Inter";
+        name = themeData.fonts.sans;
       };
       sizes = {
-        terminal = lib.mkDefault vars.fontSize;
+        terminal = lib.mkDefault themeData.fonts.size;
         applications = 11;
       };
     };
 
     cursor = {
       package = pkgs.apple-cursor;
-      name = lib.mkDefault vars.cursorTheme;
-      size = lib.mkDefault vars.cursorSize;
+      name = lib.mkDefault themeData.cursor.name;
+      size = lib.mkDefault themeData.cursor.size;
     };
 
     targets.grub.enable = false;
   };
+
+  # auto-update wallpaper symlink on rebuild
+  home.activation.linkWallpapers = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    mkdir -p "${builtins.unsafeDiscardStringContext (toString ~/Wallpapers)}"
+    ln -sfn "${themeDir}/wallpapers" "${builtins.unsafeDiscardStringContext (toString ~/Wallpapers)}/current"
+  '';
 
   home-manager.users.${vars.username}.stylix.targets = {
     vscode.enable = lib.mkDefault false;
