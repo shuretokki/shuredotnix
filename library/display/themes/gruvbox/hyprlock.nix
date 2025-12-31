e7813fb4a6b69a0b2efef66a6f49e39b4060d104
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-31 23:10:33 +0700

    add hyprlock theme

---
+++ library/display/themes/gruvbox/hyprlock.nix
@@ -0,0 +1,11 @@
+{ colors, ... }: {
+  fontFamily = "SF Pro Rounded";
+  fontSize = 64;
+
+  inputField = {
+    size = "300, 50";
+    outlineThickness = 2;
+    dotsSize = 0.33;
+    dotsSpacing = 0.15;
+  };
+}
