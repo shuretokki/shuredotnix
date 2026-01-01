d06fac93974696f6e5bbe0e2ab3c91e490096f7d
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-01 19:58:41 +0700

    clean vars nix

--- vars.nix
+++ vars.nix
@@ -1,26 +1,14 @@
 {
   username = "shure";
-  hostname = "shure";
-  gitname = "Tri R. Utomo";
-  email = "tri.r.utomo@gmail.com";
 
-  browser = "zen";              # zen, firefox, chromium
-  terminal = "warp-terminal";   # warp-terminal, kitty, alacritty
-  editor = "code";              # code, nvim
-  fileManager = "nautilus";     # nautilus
-  musicPlayer = "spotify";      # spotify, youtube-music
 
-  theme = "gruvbox";            # gruvbox, catppuccin, custom
-  wallpaperDir = "Wallpapers";
-  cursorTheme = "macOS";
-  cursorSize = 24;
+  browser = "zen"; # zen, firefox, chromium
+  terminal = "warp-terminal"; # warp-terminal, kitty, alacritty
+  editor = "code"; # code, nvim
+  fileManager = "nautilus"; # nautilus
+  musicPlayer = "spotify"; # spotify, youtube-music
 
-  fontMono = "JetBrainsMono Nerd Font";
-  fontSans = "SF Pro Rounded";
-  fontSize = 12;
-
-  timezone = "Asia/Jakarta";
-  locale = "en_US.UTF-8";
+  theme = "gruvbox"; # default, gruvbox
 
   features = {
     cli = true;
