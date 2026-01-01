f9875a6826d9a68f4cde16d6f1ff429b6448555c
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-01 20:02:19 +0700

    update hyprlock config

--- library/display/hyprlock/default.nix
+++ library/display/hyprlock/default.nix
@@ -1,17 +1,6 @@
 { config, pkgs, lib, vars, ... }:
 let
   colors = config.lib.stylix.colors;
-
-  themeDir = ../themes + "/${vars.theme}";
-  themeHyprlock = if builtins.pathExists (themeDir + "/hyprlock.nix")
-                  then import (themeDir + "/hyprlock.nix") { inherit colors; }
-                  else {};
-
-  fontFamily = themeHyprlock.fontFamily or "SF Pro Rounded";
-  fontSize = themeHyprlock.fontSize or 64;
-  inputSize = themeHyprlock.inputField.size or "300, 50";
-  inputOutline = themeHyprlock.inputField.outlineThickness or 2;
-  inputDotsSize = themeHyprlock.inputField.dotsSize or 0.33;
 in {
   programs.hyprlock = {
     enable = true;
@@ -33,10 +22,10 @@ in {
       input-field = lib.mkForce [
         {
           monitor = "";
-          size = inputSize;
-          outline_thickness = inputOutline;
-          dots_size = inputDotsSize;
-          dots_spacing = 0.15;
+          size = config.theme.hyprlock.input-field.size;
+          outline_thickness = config.theme.hyprlock.input-field.outline_thickness;
+          dots_size = config.theme.hyprlock.input-field.dots_size;
+          dots_spacing = config.theme.hyprlock.input-field.dots_spacing;
           dots_center = true;
           outer_color = "rgba(${colors.base0D}ff)";
           inner_color = "rgba(${colors.base00}ff)";
@@ -55,8 +44,8 @@ in {
           monitor = "";
           text = "$TIME";
           color = "rgba(${colors.base05}ff)";
-          font_size = fontSize;
-          font_family = fontFamily;
+          font_size = config.theme.hyprlock.fontSize;
+          font_family = config.theme.hyprlock.fontFamily;
           position = "0, 80";
           halign = "center";
           valign = "center";
