9007b6ce1ae0672dacda2a87d6137141a12e10b5
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-03 15:05:14 +0700

    update git options

--- library/home/bundlers/cli/git.nix
+++ library/home/bundlers/cli/git.nix
@@ -1,43 +1,18 @@
-{ lib, vars, ... }:
-let
-  git = if builtins.pathExists ../../../git.local.nix
-        then import ../../../git.local.nix
-        else { inherit (vars) gitname email; };
+# https://github.com/nix-community/home-manager/blob/master/modules/programs/git.nix
 
-  isPlaceholder =
-    git.gitname == "Your Name" ||
-    git.email == "you@example.com" ||
-    git.gitname == "" ||
-    git.email == "";
-in {
-  programs.git = lib.mkIf (!isPlaceholder) {
+{ lib, vars, ... }: {
+  programs.git = {
     enable = true;
-    lfs.enable = true;
-    userName = git.gitname;
-    userEmail = git.email;
+    settings = {
+      user.name = vars.gitname;
+      user.email = vars.email;
 
-    extraConfig = {
-      init.defaultBranch = "main";
       push.autoSetupRemote = true;
       pull.rebase = true;
-      credential.helper = "!gh auth git-credential";
-    };
 
-    aliases = {
-      co = "checkout";
-      st = "status";
-      lg = "log --oneline --graph --decorate";
-      last = "log -1 HEAD";
+      alias = {
+        last = "log -1 HEAD";
+      };
     };
   };
-
-  warnings = lib.optional isPlaceholder ''
-    Git identity not configured!
-
-    Your git config uses placeholder values or is empty.
-    To fix: Configure vars.nix or create git.local.nix at repo root with:
-      { gitname = "Your Name"; email = "your@email.com"; }
-
-    Git config will be SKIPPED until this is resolved.
-  '';
 }
