5676d74185a4bab73399a6232739478247f7b9a8
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-03 23:08:57 +0700

    cleaning variable

--- library/home/bundlers/desktop/mime.nix
+++ library/home/bundlers/desktop/mime.nix
@@ -1,15 +1,15 @@
 { lib, vars, ... }:
 let
-  browserDesktop = if vars.browser == "zen" then "zen-beta" else vars.browser;
+  browser = if vars.browser == "zen" then "zen-beta" else vars.browser;
 in {
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      "text/html" = lib.mkForce "${browserDesktop}.desktop";
-      "x-scheme-handler/http" = lib.mkForce "${browserDesktop}.desktop";
-      "x-scheme-handler/https" = lib.mkForce "${browserDesktop}.desktop";
-      "x-scheme-handler/about" = lib.mkForce "${browserDesktop}.desktop";
-      "x-scheme-handler/unknown" = lib.mkForce "${browserDesktop}.desktop";
+      "text/html" = lib.mkForce "${browser}.desktop";
+      "x-scheme-handler/http" = lib.mkForce "${browser}.desktop";
+      "x-scheme-handler/https" = lib.mkForce "${browser}.desktop";
+      "x-scheme-handler/about" = lib.mkForce "${browser}.desktop";
+      "x-scheme-handler/unknown" = lib.mkForce "${browser}.desktop";
     };
   };
 }
