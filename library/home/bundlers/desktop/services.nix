94dad1bf22a40d9ad1b620c1c78cd483ca23bde7
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-03 23:09:18 +0700

    remove easyeffects

--- library/home/bundlers/desktop/services.nix
+++ library/home/bundlers/desktop/services.nix
@@ -2,11 +2,9 @@
   services.network-manager-applet.enable = lib.mkDefault true;
   services.blueman-applet.enable = lib.mkDefault true;
   services.udiskie.enable = lib.mkDefault true;
-  services.easyeffects.enable = lib.mkDefault true;
   services.playerctld.enable = lib.mkDefault true;
 
   home.packages = with pkgs; [
     pavucontrol
-    # easyeffects is installed by services.easyeffects
   ];
 }
