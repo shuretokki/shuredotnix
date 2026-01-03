611cd6f05d8577330de601fecc9fb8106535252e
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-03 19:33:00 +0700

    update bat config

--- library/home/bundlers/cli/bat.nix
+++ library/home/bundlers/cli/bat.nix
@@ -1,8 +1,29 @@
-{ lib, ... }: {
+# https://github.com/nix-community/home-manager/blob/master/modules/programs/bat.nix
+
+{
+  lib,
+  pkgs,
+  ...
+}:
+{
   programs.bat = {
     enable = lib.mkDefault true;
+
     config = {
-      style = lib.mkDefault "numbers,changes";
+      style = "numbers,changes,header";
+      pager = "less -FR";
+      italic-text = "always";
+      wrap = "auto";
     };
+
+    extraPackages = with pkgs.bat-extras; [
+      batdiff
+      batman
+      batgrep
+      batwatch
+    ];
+
+    themes = { };
+    syntaxes = { };
   };
 }
