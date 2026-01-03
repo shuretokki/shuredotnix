39dc61153e709ab94ada3d40600c0f4eddd37198
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-03 19:50:54 +0700

    update audio config

--- library/core/audio.nix
+++ library/core/audio.nix
@@ -1,13 +1,81 @@
-{ pkgs, ... }: {
-  # Enable sound with pipewire.
+# https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/services/desktops/pipewire/pipewire.nix
+# https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/security/rtkit.nix
+
+{ pkgs, ... }:
+{
+  # Disable PulseAudio in favor of PipeWire
+  # PipeWire provides PulseAudio compatibility via pipewire-pulse
   services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
+
+  # RealtimeKit, D-Bus service that hands out realtime scheduling priority
+  # Required by PipeWire and PulseAudio for low-latency audio
+  security.rtkit = {
+    enable = true;
+    # Optional: Customize rtkit-daemon priority settings
+    # args = [ "--our-realtime-priority=29" "--max-realtime-priority=28" ];
+  };
+
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+
+    # Use PipeWire as the primary sound server
+    # Automatically set when alsa, jack, or pulse is enabled
+    audio.enable = true;
+
+    # Start PipeWire when apps connect
+    socketActivation = true;
+
+    # ALSA support - required for most Linux applications
+    alsa = {
+      enable = true;
+      # Enable 32-bit ALSA support on 64-bit systems
+      # Required for 32-bit applications (games, wine, etc.)
+      support32Bit = true;
+    };
+
+    # PulseAudio server emulation
+    # Allows PulseAudio applications to work with PipeWire
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+
+    # JACK audio emulation for professional audio applications
+    # Enables low-latency audio for DAWs like Ardour, Bitwig, etc.
+    jack.enable = false;
+
+    # Opens UDP/6001-6002 for RAOP/AirPlay timing and control data
+    raopOpenFirewall = false;
+
+    # Additional PipeWire configuration drop-ins
+    # Each item becomes a file in /etc/pipewire/*.conf.d/
+    extraConfig = {
+      # Main PipeWire server configuration
+      # See: https://gitlab.freedesktop.org/pipewire/pipewire/-/wikis/Config-PipeWire
+      pipewire = {
+        # "10-clock-rate" = {
+        #   "context.properties" = {
+        #     "default.clock.rate" = 48000;
+        #     "default.clock.allowed-rates" = [ 44100 48000 ];
+        #   };
+        # };
+      };
+
+      # Client library configuration (affects most applications)
+      # See: https://gitlab.freedesktop.org/pipewire/pipewire/-/wikis/Config-client
+      client = { };
+
+      # JACK server/client configuration
+      # See: https://gitlab.freedesktop.org/pipewire/pipewire/-/wikis/Config-JACK
+      jack = { };
+
+      # PulseAudio server emulation configuration
+      # See: https://gitlab.freedesktop.org/pipewire/pipewire/-/wikis/Config-PulseAudio
+      pipewire-pulse = { };
+    };
+
+    # Wireplumber session manager (enabled by default)
+    wireplumber = {
+      enable = true;
+      # Additional wireplumber configuration packages
+      # configPackages = [ ];
+    };
   };
 }
