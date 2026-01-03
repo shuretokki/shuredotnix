cb374d721dc220678c40f7ea8f1403309240de52
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-03 19:31:23 +0700

    update direnv config

--- library/home/bundlers/cli/direnv.nix
+++ library/home/bundlers/cli/direnv.nix
@@ -1,7 +1,31 @@
-{ lib, ... }: {
+# https://github.com/nix-community/home-manager/blob/master/modules/programs/direnv.nix
+
+{
+  lib,
+  ...
+}:
+{
   programs.direnv = {
     enable = lib.mkDefault true;
-    enableBashIntegration = lib.mkDefault true;
-    nix-direnv.enable = lib.mkDefault true;
+    enableBashIntegration = true;
+
+    nix-direnv.enable = true;
+
+    silent = false;
+
+    config = {
+      global = {
+        warn_timeout = "30s";
+        hide_env_diff = false;
+      };
+      whitelist = {
+        prefix = [ ];
+        exact = [ ];
+      };
+    };
+
+    stdlib = ''
+      # custom direnv stdlib
+    '';
   };
 }
