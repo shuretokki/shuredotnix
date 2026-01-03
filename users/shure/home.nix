743466e889188529abfd0f602295fb9cc1fde8d5
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-03 15:06:17 +0700

    remove bash options

--- users/shure/home.nix
+++ users/shure/home.nix
@@ -6,20 +6,4 @@
     home.username = vars.username;
     home.homeDirectory = "/home/${vars.username}";
     home.stateVersion = "25.11";
-
-    programs.bash = {
-        enable = true;
-        shellAliases = {
-            rebuild = "nh os switch";
-            rebuild-test = "nh os test";
-            update = "nh os switch --update";
-            clean = "nh clean all --keep 5";
-            gs = "git status";
-            gd = "git diff";
-            ga = "git add";
-            gc = "git commit";
-            gp = "git push";
-            gl = "git log";
-        };
-    };
 }
