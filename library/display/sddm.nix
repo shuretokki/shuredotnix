767b08d4f72092bcbc70bd7a29ca7037e21ca155
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-03 12:54:25 +0700

    renamed sddm-config to sddm and better implementation

---
+++ library/display/sddm.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, vars, ... }:
+let
+  sugarDark = pkgs.sddm-sugar-dark.override {
+    themeConfig.Background = "${config.theme.wallpaper}";
+  };
+in {
+  services.displayManager.sddm = {
+    enable = true;
+    wayland = {
+      enable = true;
+      compositor = "weston";
+    };
+
+    autoNumlock = true;
+    theme = "sugar-dark";
+
+    settings = {
+      Theme = {
+        CursorTheme = config.theme.cursor.name;
+        CursorSize = config.theme.cursor.size;
+      };
+    };
+
+    # autoLogin = {
+    #   relogin = false;
+    # };
+
+    package = pkgs.kdePackages.sddm;
+    extraPackages = [ sugarDark ];
+  };
+}
