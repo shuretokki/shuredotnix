e620b463c7c227c74d68081cc5575ef980144ca9
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-04 20:54:55 +0700

    add zen browser policies

--- library/home/bundlers/desktop/browser.nix
+++ library/home/bundlers/desktop/browser.nix
@@ -1,7 +1,29 @@
 { lib, pkgs, inputs, vars, ... }: {
   imports = [ inputs.zen-browser.homeModules.beta ];
 
-  programs.zen-browser.enable = lib.mkDefault true;
+  programs.zen-browser = {
+    enable = lib.mkDefault true;
+
+    policies = {
+      AutofillAddressEnabled = true;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+    };
+  };
+
   programs.chromium = {
     enable = lib.mkDefault true;
     extensions = [
