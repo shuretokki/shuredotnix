97992568187f433cd2d88c77fe14a306e8c82a2e
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-04 21:47:21 +0700

    update ci cache configuration

--- flake.nix
+++ flake.nix
@@ -3,10 +3,12 @@
 
   nixConfig = {
     extra-substituters = [
+      "https://shuredotnix.cachix.org"
       "https://hyprland.cachix.org"
       "https://vicinae.cachix.org"
     ];
     extra-trusted-public-keys = [
+      "shuredotnix.cachix.org-1:rmlHcxqncZqjzGscGzHhYUctuGc3bQEte7Lh1PkO0Xc="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
     ];
