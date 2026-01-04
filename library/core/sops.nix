f7ee9f26b58f85851e8d3d8a5b42730d3166ee91
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-04 20:54:49 +0700

    enhance sops configuration

--- library/core/sops.nix
+++ library/core/sops.nix
@@ -1,10 +1,36 @@
-{ inputs, lib, ... }: {
+# https://github.com/Mic92/sops-nix
+#
+# Usage:
+# 1. Generate your own age key: age-keygen -o ~/.config/sops/age/keys.txt
+# 2. Get your public key: age-keygen -y ~/.config/sops/age/keys.txt
+# 3. Update .sops.yaml with your public key
+# 4. Create secrets/secrets.yaml: sops secrets/secrets.yaml
+
+{ inputs, config, lib, vars, ... }:
+
+let
+  username = vars.username;
+in
+{
   imports = [ inputs.sops-nix.nixosModules.sops ];
 
   sops = {
     defaultSopsFile = lib.mkDefault ../../secrets/secrets.yaml;
-    age.keyFile = lib.mkDefault "/home/shure/.config/sops/age/keys.txt";
+    defaultSopsFormat = "yaml";
 
-    # secrets.github_token = {};
+    age = {
+      keyFile = lib.mkDefault "/home/${username}/.config/sops/age/keys.txt";
+      # sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      # generateKey = true;
+    };
+
+    secrets = {
+      # github_token = {};
+      # user_password = { neededForUsers = true; };
+    };
+
+    # templates."config.toml".content = ''
+    #   password = "${config.sops.placeholder.some_secret}"
+    # '';
   };
 }
