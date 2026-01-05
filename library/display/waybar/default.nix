94473ae5a3bfc6b0e3044ef258ec121cd4405046
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-05 17:57:03 +0700

    update waybar
    fix waybar style units
    
    fix waybar config structure

--- library/display/waybar/default.nix
+++ library/display/waybar/default.nix
@@ -2,18 +2,12 @@
 let
   cfg = config.theme.waybar;
 in {
-  # Logic to handle waybar if enabled?
-  # Assuming this module is imported when waybar is desired.
-
   programs.waybar = {
     enable = true;
     systemd.enable = true;
-    # Disable HM generation of these files since we provide them manually
     settings = {};
-    style = "";
   };
 
-  # Direct file provisioning
   xdg.configFile = {
     "waybar/config.jsonc" = lib.mkIf (cfg.configFile != null) {
       source = cfg.configFile;
@@ -21,5 +15,23 @@ in {
     "waybar/style.css" = lib.mkIf (cfg.styleFile != null) {
       source = cfg.styleFile;
     };
+    "waybar/colors.css".text = ''
+      @define-color base00 #${config.lib.stylix.colors.base00};
+      @define-color base01 #${config.lib.stylix.colors.base01};
+      @define-color base02 #${config.lib.stylix.colors.base02};
+      @define-color base03 #${config.lib.stylix.colors.base03};
+      @define-color base04 #${config.lib.stylix.colors.base04};
+      @define-color base05 #${config.lib.stylix.colors.base05};
+      @define-color base06 #${config.lib.stylix.colors.base06};
+      @define-color base07 #${config.lib.stylix.colors.base07};
+      @define-color base08 #${config.lib.stylix.colors.base08};
+      @define-color base09 #${config.lib.stylix.colors.base09};
+      @define-color base0A #${config.lib.stylix.colors.base0A};
+      @define-color base0B #${config.lib.stylix.colors.base0B};
+      @define-color base0C #${config.lib.stylix.colors.base0C};
+      @define-color base0D #${config.lib.stylix.colors.base0D};
+      @define-color base0E #${config.lib.stylix.colors.base0E};
+      @define-color base0F #${config.lib.stylix.colors.base0F};
+    '';
   };
 }
