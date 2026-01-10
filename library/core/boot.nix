bb7a77442a8bbe89fb0826c732ce8c070dc6c8db
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-10 10:35:10 +0700

    docs(core:boot): add macos dual boot note and reformat comments
    - Add macOS EFI path note for future dual boot setup
    - Reformat secure boot comment for readability
    - Note that macOS path is unverified

--- library/core/boot.nix
+++ library/core/boot.nix
@@ -18,8 +18,8 @@
 
     # this requires you to already have generated the keys and enrolled them with sbctl.
     # to create keys use 'sbctl create-keys'.
-    # to enroll them first reset secure boot to “Setup Mode”.
-    # this is device specific. then enroll them using 'sbctl enroll-keys -m -f'.
+    # to enroll them first reset secure boot to “Setup Mode”. this is device specific.
+    # then enroll them using 'sbctl enroll-keys -m -f'.
     secureBoot.enable = false;
 
     # maximum number of system generations to display in the boot menu.
@@ -33,6 +33,8 @@
     # limine on NixOS does not have 'osProber' (unlike GRUB).
     # you must manually add entries for other OSs (Dual Boot).
     # default windows path: boot():/EFI/Microsoft/Boot/bootmgfw.efi
+    # for macos might be boot():/EFI/BOOT/bootx64.efi or /System/Library/CoreServices/boot.efi
+    # since i can't verify it yet, i willn't add it in
     extraEntries = ''
       /Windows
         protocol: efi_chainload
